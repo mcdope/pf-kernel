76bdd6350d8ea8435db4243a56d89c1eefbc1dd7
Tobias Bäumer <tobiasbaeumer@gmail.com>
2013-11-01 12:23:19 +0100

    Updated packaging script

--- mcbuild.sh
+++ mcbuild.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-rm ../linux* && rm -rf ./debian && make mrproper && cp ./configs/dell-vostro-2520.config ./.config && make-kpkg --initrd buildpackage --append_to_version="-mcdope+`git log -1 --format="%H"`"
+echo "==> Cleaning packaging files..."
+rm ../linux* > /dev/null
+rm -rf ./debian > /dev/null
+
+echo "==> Cleaning source tree..."
+make mrproper 
+
+echo "==> Copying Vostro 2520 config..."
+cp ./configs/dell-vostro-2520.config ./.config
+
+echo "==> Building .deb packages..."
+make-kpkg --initrd --append_to_version="-mcdope+`git log -1 --format="%H"`" kernel_image kernel_headers kernel_source
+
+echo ""
+echo ""
+echo "==> DONE!"
+
