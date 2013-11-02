81b4d91fc1f936924eb7f2749d427042757dbe95
Tobias Bäumer <tobiasbaeumer@gmail.com>
2013-11-02 21:31:23 +0100

    Build script > mrproper is now optional, script need to be invoked with "clean" param

--- mcbuild.sh
+++ mcbuild.sh
@@ -3,15 +3,19 @@ echo "==> Cleaning packaging files..."
 rm ../linux* > /dev/null
 rm -rf ./debian > /dev/null
 
-echo "==> Cleaning source tree..."
-make mrproper 
+if [ "$1" == "clean" ] 
+    then
+	echo "==> Cleaning source tree..."
+	make mrproper 
+fi
 
 echo "==> Copying Vostro 2520 config..."
 cp ./configs/dell-vostro-2520.config ./.config
 
 echo "==> Building .deb packages..."
-make-kpkg --initrd --append_to_version="-mcdope+`git log -1 --format="%H"`" kernel_image kernel_headers kernel_source
-
+#make-kpkg --initrd --append_to_version="-mcdope+`git log -1 --format="%H"`" kernel_image kernel_headers kernel_source
+#make-kpkg --initrd --append_to_version="+`git log -1 --format="%H"`" kernel_image kernel_headers kernel_source
+make-kpkg --initrd --append_to_version="+mcdope-`date +%Y%m%d-%H%M`" kernel_image kernel_headers kernel_source
 echo ""
 echo ""
 echo "==> DONE!"
