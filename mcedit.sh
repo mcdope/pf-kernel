3e7aebb1073b65f6782d484237c622e17dc4a9ec
Tobias Bäumer <tobiasbaeumer@gmail.com>
2013-11-02 19:08:19 +0100

    Small shell script for editing the config...

---
+++ mcedit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo '==> Grabbing config'
+cp configs/dell-vostro-2520.config .config
+echo '==> make menuconfig'
+make menuconfig
+echo '==> Saving config'
+cp .config /configs/dell-vostro-2520.config
+echo 'DONE!'
+
