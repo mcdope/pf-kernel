22d95b03db8b95c57052d19be4f69ec8b868b759
Tobias Bäumer <tobiasbaeumer@gmail.com>
2013-11-11 10:21:51 +0100

    Build/packaging script > added logging of build
    Conflicts:
    	mcbuild.sh

---
+++ mcbuild.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "==> Cleaning packaging files..."
+rm ../linux* > /dev/null
+rm -rf ./debian > /dev/null
+
+if [ "$1" == "clean" ] 
+    then
+	echo "==> Cleaning source tree..."
+	make mrproper 
+fi
+
+echo "==> Copying Vostro 2520 config..."
+cp ./configs/dell-vostro-2520.config ./.config
+
+echo "==> Building .deb packages..."
+#make-kpkg --initrd --append_to_version="-mcdope+`git log -1 --format="%H"`" kernel_image kernel_headers kernel_source
+#make-kpkg --initrd --append_to_version="+`git log -1 --format="%H"`" kernel_image kernel_headers kernel_source
+make-kpkg --initrd --append_to_version="+mcdope-`date +%Y%m%d-%H%M`" kernel_image kernel_headers kernel_source > build.log 2>&1
+echo ""
+echo ""
+echo "==> DONE!"
+
