baac8c74a0144d91dd903e2ca0fb9f5248a9a91c
TobiasBaeumer@gmail.com <TobiasBaeumer@gmail.com>
2014-02-22 11:14:19 +0100

    Build script > 	- removal of debian packaging files do not show an error message anymore if files are not present 	- number of jobs is determined from /proc/cpuinfo 	- total runtime measured & displayed now 	- config to use determined by hostname

--- mcbuild.sh
+++ mcbuild.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
+start_time=`date +%s`
+
 echo "==> Cleaning packaging files..."
-rm ../linux* > /dev/null
-rm -rf ./debian > /dev/null
+rm ../linux* &> /dev/null
+rm -rf ./debian &> /dev/null
 
 if [ "$1" == "clean" ] 
-    then
+then
 	echo "==> Cleaning source tree..."
 	make mrproper 
 fi
 
-echo "==> Copying Vostro 2520 config..."
-cp ./configs/dell-vostro-2520.config ./.config
+HOST=`hostname`
+if [ "$HOST" == "tbits-laptop" ] 
+then
+	echo "==> Copying Laptop (Dell Vostro 2520) config..."
+	cp ./configs/dell-vostro-2520.config ./.config
+fi
+if [ "$HOST" == "dope-seiner" ] 
+then
+	echo "==> Copying Desktop (ASUS P8-Z77M + i5 3570k) config..."
+	cp ./configs/mcdope-desktop.config ./.config
+fi
 
-echo "==> Building .deb packages..."
-#make-kpkg --initrd --append_to_version="-mcdope+`git log -1 --format="%H"`" kernel_image kernel_headers kernel_source
-#make-kpkg --initrd --append_to_version="+`git log -1 --format="%H"`" kernel_image kernel_headers kernel_source
-make-kpkg --initrd --append_to_version="+mcdope-`date +%Y%m%d-%H%M`" kernel_image kernel_headers kernel_source > build.log 2>&1
+mccores=`grep -c "cpu cores" /proc/cpuinfo`
+echo -n "==> Building .deb packages with "
+echo -n $mccores
+echo " parallel jobs..."
+make-kpkg -j$mccores --initrd --append_to_version="+mcdope-`date +%Y%m%d-%H%M`" kernel_image kernel_headers kernel_source > build.log 2>&1
 if [ "$1" == "install" ]
     then
 	echo "==> Installing .deb packages ..."
 	sudo dpkg -i ../linux-*
 fi
+
 echo ""
 echo ""
-echo "==> DONE!"
+end_time=`date +%s`
+time_diff=`expr $end_time - $start_time`
+echo -n "==> DONE in "
+echo -n $time_diff 
+echo " s!"
 
