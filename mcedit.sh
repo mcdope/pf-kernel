09ab43983bfa115b094cf98003eb36009178e546
Tobias Bäumer <tobiasbaeumer@gmail.com>
2016-04-24 08:22:47 +0200

    scripts: mcedit: enabled core/thread count detection

--- mcedit.sh
+++ mcedit.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+mccores=`grep -c "cpu cores" /proc/cpuinfo`
+
 echo '==> Grabbing config '$1
 cp $1 .config
 echo '==> make menuconfig'
-make menuconfig
+make -j$mccores menuconfig
 echo '==> Saving config'
 cp .config $1
 echo 'DONE!'
